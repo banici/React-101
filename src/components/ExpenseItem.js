a2ed168503ada3e95aebf075826ecd54771077c6
banici <banicworld@gmail.com>
2021-08-20 15:38:07 +0200

    dynamic components inserted

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+
   return (
-    <div className="ExpenseItem">
-      <div>Date</div>
+    <div className="expense-item">
       <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
